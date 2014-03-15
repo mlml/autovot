e31049906bbef6a2eafa07603a31f96fdc35699d
msonderegger <diomede@nowhere.com>
2014-03-15 18:20:16 -0400

    checked / changed prose

--- auto_vot/bin/auto_vot_train.py
+++ auto_vot/bin/auto_vot_train.py
@@ -2,6 +2,9 @@
 
 """
 auto_vot_train.py
+
+Train a classifier to automatically measure VOT, using manually annotated VOTs in a set of textgrids and corresponding wav files. Run with -h argument for information on arguments, and see documentation for usage examples.
+
 Author: Joseph Keshet, 18/11/2013
 """
 
@@ -15,28 +18,33 @@ from autovot.utilities import *
 
 if __name__ == "__main__":
     # parse arguments
-    parser = argparse.ArgumentParser(description='Training of AutoVOT')
-    parser.add_argument('wav_list', help="list of WAV files")
-    parser.add_argument('textgrid_list', help="list of manually labeled TextGrid files")
-    parser.add_argument('model_filename', help="output model file name")
-    parser.add_argument('--vot_tier', default='vot', help='name of the tier to extract VOTs from')
-    parser.add_argument('--vot_mark', default='*', help='VOT mark value (e.g., "pos", "neg") or "*" for any string')
-    parser.add_argument('--window_min', help='window left boundary (in msec) relative to the VOT right boundary ('
-                                              'usually should be negative, that is, before the VOT right boundary.)',default=-0.05, type=float)
-    parser.add_argument('--window_max', help='window right boundary (in msec) relative to the VOT right boundary ( '
-                        'usually should be positive, that is, after the VOT left boundary.)', default=0.8, type=float)
-    parser.add_argument('--cv_auto', help='use 20%% of the training set for cross validation', action='store_true', default=False)
-    parser.add_argument('--cv_textgrid_list', default='', help='list of manually labeled TextGrid files for '
-                                                                'cross-validation')
-    parser.add_argument('--cv_wav_list', default='', help='list of WAV files for cross-validation')
-    parser.add_argument('--max_num_instances', default=0, type=int, help='max number of instances per file to use ('
-                                                                          'default is to use everything)')
-    parser.add_argument("--logging_level", help="print out level (DEBUG, INFO, WARNING or ERROR)", default="INFO")
+    parser = argparse.ArgumentParser(description='Train a classifier to automatically measure VOT, using manually annotated VOTs in a set of textgrids and corresponding wav files.  See documentation for usage examples.')
+    parser.add_argument('wav_list', help="Text file listing  WAV files")
+    parser.add_argument('textgrid_list', help="Text file listing corresponding manually labeled TextGrid files")
+    parser.add_argument('model_filename', help="Name of classifiers which are outputted")
+    parser.add_argument('--vot_tier', default='vot', help='Name of the tier to extract VOTs from (default: %(default)s)')
+    parser.add_argument('--vot_mark', default='*', help='Only intervals on the vot_tier with this mark value (e.g. "vot", "pos", "neg") are used for training, or "*" for any string (this is the default)')
+    parser.add_argument('--window_min', help='Left boundary of the window (in seconds) relative to the VOT interval\'s right boundary.'
+                                              ' Usually should be negative, that is, before the VOT interval\'s left boundary. (default: %(default)s)', default = -0.05, type=float)
+    parser.add_argument('--window_max', help='Right boundary of the window (in seconds) relative to the VOT interval\'s right boundary.'
+                        'Usually should be positive, that is, after the VOT interval\'s right boundary. (default: %(default)s)', default=0.8, type=float)
+    parser.add_argument('--cv_auto', help='Use 20%% of the training set for cross-validation (default: don\'t do this)',
+                        action='store_true', default=False)
+    parser.add_argument('--cv_wav_list', default='', help='Text file listing WAV files for cross-validation (default: none)')
+    parser.add_argument('--cv_textgrid_list', default='', help='Text file listing corresponding manually labeled TextGrid files for '
+                                                                'cross-validation (default: none)')
+    parser.add_argument('--max_num_instances', default=0, type=int, help='Maximum number of instances per file to use ('
+                                                                          'default: use everything)')
+    parser.add_argument("--logging_level", help="Level of verbosity of information printed out by this program (DEBUG, INFO, WARNING or ERROR), in order of increasing verbosity. See http://docs.python.org/2/howto/logging for definitions. (default: %(default)s)", default="INFO")
     args = parser.parse_args()
 
     logging_defaults(args.logging_level)
 
-    # intermediate files that will be used to represent the locations of the VOTs, their windows and the features
+    # intermediate files that will be used to represent the locations
+    # of the VOTs, their windows and the location of the corresponding
+    # feature files.  This is all kept in a working directory, which
+    # is deleted at the end of the script.
+    
     working_dir = tempfile.mkdtemp()
     features_dir = working_dir + "/features"
     basename = working_dir + "/training"
@@ -48,7 +56,7 @@ if __name__ == "__main__":
     tier_definitions = TierDefinitions()
     tier_definitions.extract_definition(args)
 
-    # prepare configuration files for the front end (means acoustic features extraction)
+    # Prepare configuration files for the front end (i.e., the acoustic feature extraction, which results in the feature files)
     problematic_files = textgrid2front_end(args.textgrid_list, args.wav_list, input_filename, features_filename,
                                            features_dir, tier_definitions)
 
@@ -57,12 +65,14 @@ if __name__ == "__main__":
                                                                labels_filename)
     easy_call(cmd_vot_front_end)
 
-    # random file lines
+    # Randomize training order of the examples.  We assume the user wants to do this, as it tends to result in better classifiers.
     features_filename_rs = features_filename + ".rs"
     labels_filename_rs = labels_filename + ".rs"
     num_instances = random_shuffle_data(features_filename, labels_filename, features_filename_rs, labels_filename_rs)
 
-    # Split files to cross-validation
+    ## Now carry out training and testing.  Three options:
+
+    # Option 1: if --cv_auto flag given, hold out 20% of the data for cross-validation, use the remaining 80% for training
     if args.cv_textgrid_list == '' and args.cv_auto:
         logging.debug("Using 20% of the data for cross-validation")
         num_instances_for_training = int(num_instances * 0.80)
@@ -84,11 +94,12 @@ if __name__ == "__main__":
         cmd_vot_training = 'InitialVotTrain -verbose %s -pos_only -vot_loss -epochs 2 -loss_eps 4 -min_vot_length 5 %s %s %s' % \
             (args.logging_level, features_filename_training, labels_filename_training, args.model_filename)
         easy_call(cmd_vot_training)
-        # Test
+        # Testing
         cmd_vot_decode = 'InitialVotDecode -verbose %s %s %s %s' % (args.logging_level, features_filename_test,
             labels_filename_test, args.model_filename)
         easy_call(cmd_vot_decode)
 
+    ## Option 2: Otherwise, if the user specified data to be used for cross-validation, use that.
     elif args.cv_textgrid_list != '':
         logging.debug("Using user data for cross-validation")
         features_filename_training = features_filename_rs
@@ -96,7 +107,7 @@ if __name__ == "__main__":
         input_filename_test = working_dir + "/training.input.test"
         features_filename_test = working_dir + "/training.feature_filelist.test"
         labels_filename_test = working_dir + "/training.labels.test"
-        # prepare configuration files for the front end (means acoustic features extraction)
+        # prepare configuration files for the front end (i.e., the acoustic feature extraction, which results in the feature files)
         problematic_files += textgrid2front_end(args.cv_textgrid_list, args.cv_wav_list, input_filename_test,
                                                 features_filename_test, features_dir, tier_definitions)
         # call front end
@@ -113,6 +124,7 @@ if __name__ == "__main__":
             labels_filename_test, args.model_filename)
         easy_call(cmd_vot_decode)
 
+    ## Option 3: Otherwise, use all data for training, and don't do any cross-validation.
     else:
         features_filename_training = features_filename_rs
         labels_filename_training = labels_filename_rs
@@ -127,7 +139,7 @@ if __name__ == "__main__":
         shutil.rmtree(path=working_dir, ignore_errors=True)
 
     if len(problematic_files):
-        logging.warning("Prediction made for all files except these ones, where something was wrong:")
+        logging.warning("Classifiers trained using all files except these ones, where something was wrong:")
         logging.warning(problematic_files)
         logging.warning("Look for lines beginning with WARNING or ERROR in the program's output to see what went "
                         "wrong.")
