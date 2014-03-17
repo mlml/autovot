587c64110c4de3202a5a3665ca62979471384854
msonderegger <diomede@nowhere.com>
2014-03-16 22:44:21 -0400

    checked / changed prose

--- auto_vot/bin/auto_vot_decode.py
+++ auto_vot/bin/auto_vot_decode.py
@@ -2,6 +2,11 @@
 
 """
 auto_vot_decode.py
+
+Use an existing classifier to measure VOT for stops in a set of
+textgrids and corresponding wav files. Run with -h argument for
+information on arguments, and see documentation for usage examples.
+
 Author: Joseph Keshet, 18/11/2013
 """
 
@@ -17,32 +22,33 @@ from autovot.textgrid import *
 
 if __name__ == "__main__":
     # parse arguments
-    parser = argparse.ArgumentParser(description='Decode AutoVOT')
-    parser.add_argument('wav_filenames', default='', help="a list of WAV files.")
-    parser.add_argument('textgrid_filenames', default='', help="a list of TextGrid files.")
-    parser.add_argument('model_filename', help="output model file name")
-    parser.add_argument('--vot_tier', help='name of the tier to extract VOTs from', default='')
-    parser.add_argument('--vot_mark', help='VOT mark value (e.g., "pos", "neg") or "*" for any string', default='*')
-    parser.add_argument('--window_tier', help='used this window as a search window for training. If not given, '
-                                              'a constant window with parameters [window_min, window_max] around the '
-                                              'manually labeled VOT will be used', default='')
-    parser.add_argument('--window_mark', help='window mark value or "*" for any string', default='*')
-    parser.add_argument('--window_min', help='window left boundary (in msec) relative to the VOT right boundary '
-                                             '(usually should be negative, that is, before the VOT right boundary.)',
-                        default=-0.05, type=float)
-    parser.add_argument('--window_max', help='window right boundary (in msec) relative to the VOT right boundary ('
-                                             'usually should be positive, that is, after the VOT left boundary.)',
-                        default=0.8, type=float)
-
-    parser.add_argument('--min_vot_length', help='minimum allowed length of predicted VOT (in msec) in decoding',
+    parser = argparse.ArgumentParser(description='Use an existing classifier to measure VOT for stops in a set of textgrids and corresponding wav files.')
+    parser.add_argument('wav_filenames', default='', help="Text file listing  WAV files")
+    parser.add_argument('textgrid_filenames', default='', help="Text file listing corresponding manually labeled TextGrid files containing stops VOT is to measured for")
+    parser.add_argument('model_filename', help="Name of classifier to be used to measure VOT")
+    parser.add_argument('--vot_tier', help='Name of the tier containing manually labeled VOTs to compare automatic measurements to (optional. default is none)', default='')
+    parser.add_argument('--vot_mark', help='On vot_tier, only intervals with this mark value (e.g. "vot", "pos", "neg") are used, or "*" for any string (this is the default)', default='*')
+    parser.add_argument('--window_tier', help='Name of the tier containing windows to be searched as possible starts'
+                                              'of the predicted VOT (default: none). If not given *and* vot_tier given'
+                                              ', a window with boundaries window_min and window_max to the left and right'
+                                              'of the manually labeled VOT will be used . NOTE: either window_tier or'
+                                              'vot_tier must be specified. If both are specified, window_tier is used, '
+                                              'and window_min and window_max are ignored.', default='')
+    parser.add_argument('--window_mark', help='VOT is only predicted for intervals on the window tier with this mark value '
+                                              '(e.g. "vot", "pos", "neg"), or "*" for any string (this is the default)', default='*')
+    parser.add_argument('--window_min', help='Left boundary of the window (in seconds) relative to the VOT interval\'s left boundary.',
+                        default = -0.05, type=float)
+    parser.add_argument('--window_max', help='Right boundary of the window (in seconds) relative to the VOT interval\'s right boundary. '
+                        'Usually should be positive, that is, after the VOT interval\'s right boundary. (default: %(default)s)', default=0.8, type=float)
+    parser.add_argument('--min_vot_length', help='Minimum allowed length of predicted VOT (in msec) (default: %(default)s)',
                         default=15, type=int)
-    parser.add_argument('--max_vot_length', help='maximum allowed length of predicted VOT (in msec) in decoding',
+    parser.add_argument('--max_vot_length', help='Maximum allowed length of predicted VOT (in msec) (default: %(default)s)',
                         default=250, type=int)
-    parser.add_argument('--max_num_instances', default=0, type=int, help='max number of instances per file to use ('
-                                                                         'default is to use everything)')
-    parser.add_argument('--ignore_existing_tiers', help='add a new AutoVOT tier to output textgrids, even if one already exists',
+    parser.add_argument('--max_num_instances', default=0, type=int, help='Maximum number of instances per file to use '
+                                                                         '(default: use everything)')
+    parser.add_argument('--ignore_existing_tiers', help='add a new AutoVOT tier to output textgrids, even if one already exists (default: don\'t do so)',
                         action='store_const', const=True, default=False)
-    parser.add_argument("--logging_level", help="print out level (DEBUG, INFO, WARNING or ERROR)", default="INFO")
+    parser.add_argument("--logging_level", help="Level of verbosity of information printed out by this program (DEBUG, INFO, WARNING or ERROR), in order of increasing verbosity. See http://docs.python.org/2/howto/logging for definitions. (default: %(default)s)", default="INFO")
     args = parser.parse_args()
 
     logging_defaults(args.logging_level)
@@ -115,18 +121,18 @@ if __name__ == "__main__":
         logging.debug("%s, %s" % (textgrid_list, wav_list))
         logging.debug("%s" % input_filename)
 
-        # call front end
+        # call front end (extract features)
         problematic_file = textgrid2front_end(textgrid_list, wav_list, input_filename, features_filename,
                                          features_dir, tier_definitions, decoding=True)
         if len(problematic_file):
             problematic_files += problematic_file
             continue
-
+        
         cmd_vot_front_end = 'VotFrontEnd2 -verbose %s %s %s %s' % (args.logging_level, input_filename, features_filename,
                                                                    labels_filename)
         easy_call(cmd_vot_front_end)
 
-        # testing
+        # decoding (i.e., generate VOT predictions)
         cmd_vot_decode = 'InitialVotDecode -verbose %s -max_onset 200 -min_vot_length %d -max_vot_length %d ' \
                          '-output_predictions %s %s %s %s' % (args.logging_level, args.min_vot_length,
                                                               args.max_vot_length, preds_filename, features_filename,
@@ -178,7 +184,10 @@ if __name__ == "__main__":
         auto_vot_tier.append(Interval(xmax_preds[-1], textgrid.xmax(), ''))
         # print xmax_preds[-1], textgrid.xmax(), ''
 
-        ## check if target textgrid already has a tier named "AutoVOT", modulo preceding or trailing spaces or case
+        ## check if target textgrid already has a tier named
+        ## "AutoVOT", modulo preceding or trailing spaces or case. If
+        ## so, action taken depends on if --ignore_existing_tiers flag
+        ## invoked
         existing_tiers = [n.strip() for n in textgrid.tierNames(case="lower") if n.strip() == 'autovot']
         if len(existing_tiers) > 0:
             logging.warning("File %s already contains a tier with the name \"AutoVOT\"" % textgrid_file)
