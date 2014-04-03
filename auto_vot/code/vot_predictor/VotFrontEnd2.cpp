d5e10cdc23710a3e91479e538e92729c6cfa2145
Joseph Keshet <joseph.keshet@biu.ac.il>
2014-04-03 14:06:03 +0300

    Changed “files” to “examples”.

--- auto_vot/code/vot_predictor/VotFrontEnd2.cpp
+++ auto_vot/code/vot_predictor/VotFrontEnd2.cpp
@@ -96,7 +96,7 @@ int main(int argc, char **argv)
 	// process each line
 	if (limit_instances < 0)
 		limit_instances = instances.size();
-	LOG(INFO) << "Processing " << _min(limit_instances,instances.size()) << " files.";
+	LOG(INFO) << "Processing " << _min(limit_instances,instances.size()) << " examples.";
 	for (unsigned int i=0; i < _min(limit_instances,instances.size()); i++) {
 		
 		LOG(DEBUG) << "Processing " << instances.file_list[i] << " (" << (i+1) << " of " << instances.size() << ")";
