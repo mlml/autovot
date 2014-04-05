9849a0592560198ec7d7cdcf0bb835067bee143b
msonderegger <diomede@nowhere.com>
2014-04-05 16:38:35 -0400

    finished version of script to evaluate labeled and predicted VOTs

--- auto_vot/bin/auto_vot_performance.py
+++ auto_vot/bin/auto_vot_performance.py
@@ -1,17 +1,21 @@
 #! /usr/bin/env python
 
 """
-performance_of_textgrids.py
-Author: Joseph Keshet, 18/11/2013
+Compute various measures of performance given a set of labeled VOTsand predicted VOTs for the same stops, optionally writing information for each stop to a CSV file.
+
+Authors: Joseph Keshet and Morgan Sonderegger
 """
 
-import argparse
-import math
+import argparse, os, csv
 import numpy as np
+import scipy.stats
+
+corr2 = scipy.stats.spearmanr
+corr1 = scipy.stats.pearsonr
+
 
 from autovot.textgrid import *
 from autovot.utilities import *
-
 
 def num_lines(filename):
     lines = 0
@@ -21,7 +25,6 @@ def num_lines(filename):
 
 
 def read_textgrid_tier(textgrid_filename, vot_tier):
-
      # read TextGrid
     textgrid = TextGrid()
     textgrid.read(textgrid_filename)
@@ -39,55 +42,34 @@ def read_textgrid_tier(textgrid_filename, vot_tier):
                 vots.append(interval)
     return vots
 
+# def remove_outliers(x_list, y_list):
+#     x_array = np.array(x_list)
+#     x_mean = np.mean(x_array)
+#     x_std = np.std(x_array)
+#     y_array = np.array(y_list)
+#     y_mean = np.mean(y_array)
+#     y_std = np.std(y_array)
 
-def remove_outliers(x_list, y_list):
-    x_array = np.array(x_list)
-    x_mean = np.mean(x_array)
-    x_std = np.std(x_array)
-    y_array = np.array(y_list)
-    y_mean = np.mean(y_array)
-    y_std = np.std(y_array)
+#     x_list_ro = list()
+#     y_list_ro = list()
+#     if len(x_list) != len(y_list):
+#         logging.error("Something's wrong. The length of the input lists is not the same.")
+#     for x, y in zip(x_list, y_list):
+#         if abs(x-x_mean) < 3.0*x_std and abs(y-y_mean) < 3.0*y_std:
+#             x_list_ro.append(x)
+#             y_list_ro.append(y)
 
-    x_list_ro = list()
-    y_list_ro = list()
-    if len(x_list) != len(y_list):
-        logging.error("Something's wrong. The length of the input lists is not the same.")
-    for x, y in zip(x_list, y_list):
-        if abs(x-x_mean) < 3.0*x_std and abs(y-y_mean) < 3.0*y_std:
-            x_list_ro.append(x)
-            y_list_ro.append(y)
-
-    return x_list_ro, y_list_ro
-
-def correlation(x_list, y_list):
-    # compute first moments
-    x_mean = sum(x_list)/float(len(x_list))
-    y_mean = sum(y_list)/float(len(y_list))
-    # compute second moments
-    sum_xy = 0
-    sum_x2 = 0
-    sum_y2 = 0
-    for x, y in zip(x_list, y_list):
-        sum_xy += (x-x_mean)*(y-y_mean)
-        sum_x2 += (x-x_mean)*(x-x_mean)
-        sum_y2 += (y-y_mean)*(y-y_mean)
-    #correlation
-    corr = sum_xy/math.sqrt(sum_x2*sum_y2)
-    return corr
+#     return x_list_ro, y_list_ro
 
 
 if __name__ == "__main__":
     # parse arguments
-    parser = argparse.ArgumentParser(description='Compute the performance between a set of predicted textgrid files '
-                                                 'and a set of labeled textgrid files.')
-    parser.add_argument('labeled_textgrid_list', help="list of manually labeled TextGrid files")
-    parser.add_argument('predicted_textgrid_list', help="list of predicted TextGrid files  (can be the same as "
-                                                        "labeled_textgrid_list when the same TextGrid is used with "
-                                                        "two different tiers to compare)")
-    parser.add_argument('labeled_vot_tier', help='name of the VOT tier of the manual labeled TextGrids',
-                        default='vot')
-    parser.add_argument('predicted_vot_tier', help='name of the VOT tier to predicted TetGrids', default='vot')
-    parser.add_argument("--debug", help="verbose printing", action='store_const', const=True, default=False)
+    parser = argparse.ArgumentParser(description='Compute various measures of performance given a set of labeled VOTsand predicted VOTs for the same stops, optionally writing information for each stop to a CSV file.')
+    parser.add_argument('labeled_textgrid_list', help="textfile listing TextGrid files containing manually labeled VOTs (one per line)")
+    parser.add_argument('predicted_textgrid_list', help="textfile listing TextGrid files containing predicted VOTs (one per line). This can be the same as labeled_textgrid_list, provided two different tiers are given for labeled_vot_tier and predicted_vot_tier.")
+    parser.add_argument('labeled_vot_tier', help='name of the tier containing manually labeled VOTs in the TextGrids in labeled_textgrid_list (default: %(default)s)', default='vot')
+    parser.add_argument('predicted_vot_tier', help='name of the tier containing automatically labeled VOTs in the TextGrids in predicted_textgrid_list (default: %(default)s)', default='AutoVOT')
+    parser.add_argument('--csvF', help='csv file to dump labeled and predicted VOT info to (default: none)')
     args = parser.parse_args()
 
     if is_textgrid(args.labeled_textgrid_list) and is_textgrid(args.predicted_textgrid_list):
@@ -116,19 +98,75 @@ if __name__ == "__main__":
     y_xmin = []
     y_xmax = []
     y_vot = []
+
+    x_files = []
+    y_files = []
+    
     for labeled_textgrid, predicted_textgrid in zip(labeled_textgrids, predicted_textgrids):
         labeled_vots = read_textgrid_tier(labeled_textgrid.strip(), args.labeled_vot_tier)
+        
         x_xmin = x_xmin + [x.xmin() for x in labeled_vots]
         x_xmax = x_xmax + [x.xmax() for x in labeled_vots]
         x_vot = x_vot + [(x.xmax()-x.xmin()) for x in labeled_vots]
+        x_files = x_files + [labeled_textgrid.strip() for x in labeled_vots]
 
         predicted_vots = read_textgrid_tier(predicted_textgrid.strip(), args.predicted_vot_tier)
         y_xmin = y_xmin + [y.xmin() for y in predicted_vots]
         y_xmax = y_xmax + [y.xmax() for y in predicted_vots]
         y_vot = y_vot + [(y.xmax()-y.xmin()) for y in predicted_vots]
+        y_files = y_files + [predicted_textgrid.strip() for x in predicted_vots]
 
-    print "xmin correlation=", correlation(x_xmin, y_xmin)
-    print "xmax correlation=", correlation(x_xmax, y_xmax)
 
-    x_vot_ro, y_vot_ro = remove_outliers(x_vot, y_vot)
-    print "vot correlation=", correlation(x_vot_ro, y_vot_ro)
+    labF = os.path.abspath(args.labeled_textgrid_list)
+    predF = os.path.abspath(args.predicted_textgrid_list)
+
+
+    print "\n\nEvaluating labeled vs. predicted VOTs, using:"
+    print "- labeled VOTs: '%s' tier in %s" % (args.labeled_vot_tier, labF)
+    print "- predicted VOTs: '%s' tier in %s" % (args.predicted_vot_tier, predF)
+
+    ## performance measure 1
+    print
+    print "Correlations (Pearson/Spearman) between predicted/labeled:"
+    print "-------------"
+
+    print "Left edge (burst onset): ", corr1(x_xmin, y_xmin)[0], "/", corr2(x_xmin, y_xmin)[0]
+    print "Right edge (voicing onset): ", corr1(x_xmax, y_xmax)[0], "/", corr2(x_xmax, y_xmax)[0]
+    print "VOTs: ", corr1(x_vot, y_vot)[0], "/", corr2(x_xmax, y_xmax)[0]
+
+    print "\n(Note: if the Pearson and Spearman correlations differ much,\nyou probably have outliers which are strongly influencing Pearson's R)\n"
+
+
+    ## numpy arrays of labeled VOTs and predicted VOTs (in sec)
+    X, Y = np.array(x_vot), np.array(y_vot)
+
+    ## performance measure 2
+    print "Mean, standard deviation of labeled/predicted difference:"
+    print "------------------------------"
+    print "VOT:", 1000*np.mean(abs(X-Y)), "msec,", 1000*np.std(abs(X-Y)), "msec\n"
+
+    ## performance measure 3
+    thresholds =  [2,5,10,15,20,25,50]
+    print "Percentage of examples with labeled/predicted VOT difference of at most:"
+    print "------------------------------"
+    for thresh in thresholds:
+        print "%d msec: " % thresh, 100*(len(X[abs(X-Y)< thresh/1000.0])/float(len(X)))
+
+
+    ## dump predicted/labeled VOT info to a CSV, for later examination in R, excel, etc.
+    if args.csvF:
+        print "\nWriting labeled / predicted VOT info to %s" % args.csvF
+        outF = open(args.csvF, 'wb')
+        
+        csvF = csv.writer(outF)
+        csvF.writerow(['filename_labeled', 'filename_predicted', 'time_in_labeledF', 'time_in_predictedF',
+                   'tier_in_labeledF', 'tier_in_predictedF', 'vot_labeled', 'vot_predicted'])
+
+        for i, xmin in enumerate(x_xmin):
+            csvF.writerow([x_files[i], y_files[i], str(xmin), str(y_xmin[i]), args.labeled_vot_tier, args.predicted_vot_tier, str(x_vot[i]), str(y_vot[i])])
+
+        outF.close()
+
+    
+
+    #np.array(x_vot
