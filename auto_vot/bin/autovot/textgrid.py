21415189bf8befdf2a5fff03ea9b0b59d53b58ba
msonderegger <diomede@nowhere.com>
2014-04-09 09:45:47 -0400

    textgrid.py license change

--- auto_vot/bin/autovot/textgrid.py
+++ auto_vot/bin/autovot/textgrid.py
@@ -1,29 +1,5 @@
-#
-# Copyright (c) 2014 Joseph Keshet, Morgan Sonderegger, Thea Knowles
-#
-# This file is part of Autovot, a package for automatic extraction of
-# voice onset time (VOT) from audio files.  NOTE: this file is a
-# slightly modified version of the textgrid.py module
-# (https://github.com/kylebgorman/textgrid/), whose license is
-# reproduced below.
-#
-# Autovot is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Autovot is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with Autovot.  If not, see
-# <http://www.gnu.org/licenses/>.
-#
-########
-#
-# original textgrid.py license
+# This file is a slightly modified version of the textgrid.py module
+# (https://github.com/kylebgorman/textgrid/), which was released under the following license:
 # (see https://github.com/kylebgorman/textgrid/blob/master/LICENSE)
 #
 # Copyright (c) 2011-2013 Kyle Gorman, Max Bane, Morgan Sonderegger
